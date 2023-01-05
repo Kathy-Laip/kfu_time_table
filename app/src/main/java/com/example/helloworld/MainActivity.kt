0e0809f8d3a76ea7ac4ba9bb11d6f663f4c9c8d0
Kathy_Laip <ekaterinaslapnikova@Ekaterinas-MacBook-Air.local>
2023-01-06 00:20:39 +0300

    new

---
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.helloworld
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
