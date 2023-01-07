30a64b71ff3c25f5a01a22c8016ceef19ca67aad
Kathy_Laip <ekaterinaslapnikova@Ekaterinas-MacBook-Air.local>
2023-01-08 02:01:14 +0300

    AccScreen

---
+++ app/src/main/java/com/example/helloworld/QuestionFragment.kt
@@ -0,0 +1,11 @@
+package com.example.helloworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QuestionFragment : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_question)
+    }
+}
--- app/src/main/java/com/example/helloworld/TimeTableFragment.kt
+++ app/src/main/java/com/example/helloworld/TimeTableFragment.kt
@@ -1,11 +1,23 @@
 package com.example.helloworld
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import io.getstream.avatarview.AvatarView
+import io.getstream.avatarview.coil.loadImage
 
 class TimeTableFragment : AppCompatActivity() {
+    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_time_table)
+
+        val avatarView: AvatarView = findViewById(R.id.avatarView)
+        val drawableRes = R.drawable.acc
+
+        avatarView.loadImage(drawableRes)
+
+        }
     }
-}
